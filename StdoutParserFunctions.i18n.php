ff8ab557e3949654f7deb4e6253354ff3a23c2d4
Thomas Jensen <thomas@uctrl.net>
2020-01-02 22:03:20 +0100

    first commit

---
+++ StdoutParserFunctions.i18n.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @license GPL-2.0-or-later
+ * @author Your Name (YourUserName)
+ */
+
+$magicWords = [];
+
+/** English
+ * @author Your Name (YourUserName)
+ */
+$magicWords['en'] = [
+    'htmlvideo' => [ 0, 'htmlvideo' ],
+    'parts-list' => [ 0, 'parts-list' ],
+];
---
+++ src/StdoutParserFunctionsHooks.php
@@ -0,0 +1,66 @@
+<?php
+class StdoutParserFunctionsHooks {
+
+    // Register any render callbacks with the parser
+    public static function onParserFirstCallInit( Parser $parser )
+    {
+        $parser->setFunctionHook( 'htmlvideo', [ self::class, 'renderHtmlVideo' ] );
+        $parser->setFunctionHook( 'parts-list', [ self::class, 'renderPartsList' ] );
+    }
+
+
+    public static function renderHtmlVideo( Parser $parser, $param1 = '' )
+    {
+        parse_str(implode('&', array_slice(func_get_args(), 2)), $params);
+
+        if (! array_key_exists('t', $params)) $params['t'] = 3;
+        if (! array_key_exists('w', $params)) $params['w'] = '640px';
+
+        if (is_numeric($params['w'])) {
+            $params['w'] = floor($params['w'] * (16/9)) . 'px';
+        }
+
+        $output = "<div style=\"max-width:${params['w']}\">";
+        $output .= "<video width=\"100%\" height=\"auto\" poster=\"https://video.stdout.no/$param1/${param1}_${params['t']}.jpg\" controls preload=\"metadata\">";
+        $output .= "<source src=\"https://video.stdout.no/$param1/$param1.webm\" type=\"video/webm; codecs=vp9,vorbis\">";
+        $output .= "<source src=\"https://video.stdout.no/$param1/$param1.mp4\" type=\"video/mp4\">";
+        $output .= "</video>";
+
+        if (array_key_exists('yt', $params)) {
+            $output .= "<small>This video is also available on <a class=\"external\" href=\"https://www.youtube.com/watch?v=${params['yt']}\">YouTube</a>.</small>";
+        }
+
+        $output .= "</div>";
+
+
+        return [ $output, 'noparse' => true, 'isHTML' => true ];
+    }
+
+
+    public static function renderPartsList( Parser $parser, $param1 = '') {
+        $json = file_get_contents('/var/lib/parts-json/' . $param1 . '.json');
+        $parts = json_decode($json, true);
+
+        $output = [];
+        $output[] = '{| class="wikitable mw-collapsible searchaux"';
+        $output[] = '! scope="col" | Qty.';
+        $output[] = '! scope="col" | Part';
+        $output[] = '|-';
+
+        foreach ($parts as $part)
+        {
+            $output[] = sprintf('|\'\'\'%s\'\'\'%s || %s',
+                strval($part['q'] + 0), // remove decimals if zero
+                $part['unit'] == 'ea' ? '' : ' <small>' . $part['unit'] . '</small>',
+                html_entity_decode($part['title'])
+            );
+
+            $output[] = '|-';
+        }
+
+        $output[] = '|}';
+
+
+        return implode("\n", $output);
+    }
+}
