4ee6141ccdb7a857b574d0d54db7c52f19424e21
Thomas Jensen <thomas@uctrl.net>
2020-11-22 21:51:40 +0100

    add mux.com and videojs implementation

--- src/StdoutParserFunctionsHooks.php
+++ src/StdoutParserFunctionsHooks.php
@@ -14,41 +14,65 @@ class StdoutParserFunctionsHooks {
     {
         #$out->addLink(['rel' => 'preconnect', 'href' => 'https://static.stdout.no', 'crossorigin' => '']);
         #$out->addLink(['rel' => 'dns-prefetch', 'href' => 'https://static.stdout.no']);
-        $out->addLink(['rel' => 'dns-prefetch', 'href' => 'https://video.stdout.no']);
+        #$out->addLink(['rel' => 'dns-prefetch', 'href' => 'https://video.stdout.no']);
         $out->addScript('<script async defer data-domain="cavelab.dev" src="https://plausible.io/js/plausible.js"></script>');
     }
 
 
     public static function renderHtmlVideo( Parser $parser, $param1 = '' )
     {
-        parse_str(implode('&', array_slice(func_get_args(), 2)), $params);
+        $video = [
+            'id="my-video"',
+            'class="video-js"',
+            'controls',
+            'preload="auto"'.
+            'width="640"',
+            'height="360"',
+            'poster="https://image.mux.com/' . $param1 . '/thumbnail.png?width=640&height=360"',
+            'data-setup="{}"'
+        ];
 
-        if (! array_key_exists('t', $params)) $params['t'] = 3;
-        if (! array_key_exists('w', $params)) $params['w'] = '640';
-        if (! array_key_exists('r', $params)) $params['r'] = '16:9';
-
-        $r = explode(':', $params['r']);
-        $w = str_replace('px', '', $params['w']);
-        $h = $w / ($r[0] / $r[1]);
-
-        $dir = $param1;
-        $file = explode('/', $param1)[0];
-
-        $output = "<div style=\"margin:1em 0\">";
-        $output .= "<video width=\"$w\" height=\"$h\" poster=\"/images/video/$dir/${file}_${params['t']}.jpg\" controls preload=\"metadata\">";
-        $output .= "<source src=\"https://video.stdout.no/$dir/$file.webm\" type=\"video/webm; codecs=vp9,vorbis\">";
-        $output .= "<source src=\"https://video.stdout.no/$dir/$file.mp4\" type=\"video/mp4\">";
+        $output = "<video ". implode(' ', $video) . ">";
+        $output .= "<source type=\"application/x-mpegURL\" src=\"https://stream.mux.com/" . $param1 . ".m3u8\">";
         $output .= "</video>";
 
-        if (array_key_exists('yt', $params)) {
-            $output .= "<small>This video is also available on <a class=\"external\" href=\"https://www.youtube.com/watch?v=${params['yt']}\">YouTube</a>.</small>";
-        }
-
-        $output .= "</div>";
-
+        $parser->getOutput()->addModules('ext.stdoutParser.video');
+        $parser->getOutput()->addModuleStyles('ext.stdoutParser.video');
 
         return [ $output, 'noparse' => true, 'isHTML' => true ];
     }
+
+
+#    public static function renderHtmlVideo_old( Parser $parser, $param1 = '' )
+#    {
+#        parse_str(implode('&', array_slice(func_get_args(), 2)), $params);
+#
+#        if (! array_key_exists('t', $params)) $params['t'] = 3;
+#        if (! array_key_exists('w', $params)) $params['w'] = '640';
+#        if (! array_key_exists('r', $params)) $params['r'] = '16:9';
+#
+#        $r = explode(':', $params['r']);
+#        $w = str_replace('px', '', $params['w']);
+#        $h = $w / ($r[0] / $r[1]);
+#
+#        $dir = $param1;
+#        $file = explode('/', $param1)[0];
+#
+#        $output = "<div style=\"margin:1em 0\">";
+#        $output .= "<video width=\"$w\" height=\"$h\" poster=\"/images/video/$dir/${file}_${params['t']}.jpg\" controls preload=\"metadata\">";
+#        $output .= "<source src=\"https://video.stdout.no/$dir/$file.webm\" type=\"video/webm; codecs=vp9,vorbis\">";
+#        $output .= "<source src=\"https://video.stdout.no/$dir/$file.mp4\" type=\"video/mp4\">";
+#        $output .= "</video>";
+#
+#        if (array_key_exists('yt', $params)) {
+#            $output .= "<small>This video is also available on <a class=\"external\" href=\"https://www.youtube.com/watch?v=${params['yt']}\">YouTube</a>.</small>";
+#        }
+#
+#        $output .= "</div>";
+#
+#
+#        return [ $output, 'noparse' => true, 'isHTML' => true ];
+#    }
 
 
     public static function renderPartsList( Parser $parser, $param1 = '') {
